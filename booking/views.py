1822fc00a011cb056d39806bb724c9a7454bd234
dmytromihalevich <mihalevicmaria502@gmail.com>
2025-06-10 21:03:57 +0300

    next_one

--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from booking.models import Room, Booking
+
 # Create your views here.
 def main_page(request):
+    rooms = Room.objects.all()
 
     context = {
         "data":"hi from django"
+        'room_list': rooms
     }
 
-    return render(request, "index.html", context)
+    return render(request, "booking/room_list.html", context)
     
